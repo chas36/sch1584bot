1e45ab0b66b12ca8317b99b945f0478d4ca15bc3
chas36 <cherkasovi36@gmail.com>
2024-01-29 17:45:46 +0300

    Типа рабочий вариант с переписаным кодом. Появляются кнопки Подтвердить выбор и выбрать еще класс

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ bot = telebot.TeleBot('6332665358:AAHXpmnyWO4yKza0GAvgcy6nCFVwETs1aaA')
 
 # Подключение к Google Sheets
 scope = 'https://spreadsheets.google.com/feeds https://www.googleapis.com/auth/drive'
-credentials = ServiceAccountCredentials.from_json_keyfile_name(r'C:\Users\Пользователь\PycharmProjects\sch1584bot\sch1584-0b47bcc851fb.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name(r"C:\Users\Анд\PycharmProjects\sch1584bot1\sch1584-0b47bcc851fb.json", scope)
+
 gc = gspread.authorize(credentials)
 
 # ID вашей таблицы
@@ -88,6 +89,52 @@ def help_command(message):
     # Отправляем сообщение со списком команд
     bot.send_message(chat_id, commands_text)
 
+def get_students_for_class(selected_class):
+    class_column = worksheet.col_values(2)[1:]
+    name_column = worksheet.col_values(1)[1:]
+
+    # Получаем индексы учеников для выбранного класса
+    indices = [i for i, class_name in enumerate(class_column) if class_name == selected_class]
+
+    # Сортируем учеников по алфавиту на основе их индексов
+    sorted_students = sorted([(name_column[i], i) for i in indices])
+
+    # Возвращаем отсортированный список имен учеников
+    return [student[0] for student in sorted_students]
+
+# Декоратор для обработки Inline-кнопок
+@bot.callback_query_handler(func=lambda call: call.data.startswith('class_'))
+def handle_class_selection(call):
+    user_id = call.from_user.id
+    selected_class = call.data.split('_')[1]
+
+    # Создаем кнопки для подтверждения и выбора еще одного класса
+    confirm_button = types.InlineKeyboardButton("Подтвердить выбор", callback_data=f"confirm_{selected_class}")
+    choose_another_button = types.InlineKeyboardButton("Выбрать еще один класс", callback_data="choose_class")
+
+    # Создаем клавиатуру с этими кнопками
+    confirm_keyboard = types.InlineKeyboardMarkup(row_width=2)
+    confirm_keyboard.add(confirm_button, choose_another_button)
+
+    # Отправляем сообщение с клавиатурой для подтверждения выбора
+    bot.send_message(call.message.chat.id, f"Вы выбрали класс: {selected_class}", reply_markup=confirm_keyboard)
+
+
+# Декоратор для обработки подтверждения выбора
+@bot.callback_query_handler(func=lambda call: call.data.startswith('confirm_'))
+def handle_confirm_selection(call):
+    user_id = call.from_user.id
+    selected_class = call.data.split('_')[1]
+
+    # Сохраняем состояние пользователя (выбранный класс)
+    save_user_state(user_id, selected_class)
+
+    # Отправляем сообщение о подтверждении выбора
+    bot.send_message(call.message.chat.id, f"Ваш класс: {selected_class}. Выбор подтвержден!")
+
+    # Вызываем функцию выбора класса для дальнейшего выбора
+    choose_class(call.message)
+
 
 # Декоратор для команды "choose_class"
 @bot.message_handler(commands=['choose_class'])
@@ -95,48 +142,58 @@ def choose_class(message):
     chat_id = message.chat.id
     user_id = message.from_user.id
 
-    # Загружаем состояние пользователя (выбранную параллель)
+    # Загружаем состояние пользователя (выбранную параллель и класс)
     selected_parallel = load_user_state(user_id)
 
-    # Создаем кнопки для выбора параллели
-    parallel_buttons = [
-        types.InlineKeyboardButton(str(i), callback_data=f"parallel_{i}") for i in range(5, 12)
-    ]
-
-    # Разбиваем кнопки по две в каждом столбце
-    parallel_keyboard = types.InlineKeyboardMarkup(row_width=2)
-    parallel_keyboard.add(*parallel_buttons)
-    # Если у пользователя уже есть выбранная параллель, отправляем сообщение об этом
     if selected_parallel:
+        # Если есть выбранная параллель, показываем ее
         bot.send_message(chat_id, f"Ваша текущая параллель: {selected_parallel}")
+        # Получаем список уникальных классов для выбранной параллели
+        class_column = worksheet.col_values(2)[1:]
+        classes_for_parallel = sorted(set(class_name for class_name in class_column if class_name.startswith(selected_parallel)))
+
+        # Создаем кнопки для выбора класса
+        class_buttons = [
+            types.InlineKeyboardButton(class_name, callback_data=f"class_{class_name}") for class_name in classes_for_parallel
+        ]
+
+        # Разбиваем кнопки по две в каждом столбце
+        class_keyboard = types.InlineKeyboardMarkup(row_width=2)
+        class_keyboard.add(*class_buttons)
+
+        # Отправляем сообщение с Inline-клавиатурой для выбора класса
+        bot.send_message(chat_id, "Выберите свой класс:", reply_markup=class_keyboard)
     else:
-    # Отправляем сообщение с Inline-клавиатурой для выбора параллели
+        # Если параллель не выбрана, предлагаем выбрать параллель
+        # Создаем кнопки для выбора параллели
+        parallel_buttons = [
+            types.InlineKeyboardButton(str(i), callback_data=f"parallel_{i}") for i in range(5, 12)
+        ]
+
+        # Разбиваем кнопки по две в каждом столбце
+        parallel_keyboard = types.InlineKeyboardMarkup(row_width=2)
+        parallel_keyboard.add(*parallel_buttons)
+
+        # Отправляем сообщение с Inline-клавиатурой для выбора параллели
         bot.send_message(chat_id, "Выберите свою параллель:", reply_markup=parallel_keyboard)
 
 
-# Декоратор для обработки Inline-кнопок
+# Декоратор для обработки выбора параллели
 @bot.callback_query_handler(func=lambda call: call.data.startswith('parallel_'))
 def handle_parallel_selection(call):
     user_id = call.from_user.id
     selected_parallel = call.data.split('_')[1]
 
-    # Обрабатываем выбор параллели
-    bot.send_message(call.message.chat.id, f"Вы выбрали параллель: {selected_parallel}")
-
     # Сохраняем состояние пользователя (выбранную параллель)
     save_user_state(user_id, selected_parallel)
 
-    # Получаем список уникальных классов из столбца "Класс" в Google Sheets
+    # Получаем список уникальных классов для выбранной параллели
     class_column = worksheet.col_values(2)[1:]
-
-    # Фильтруем классы по выбранной параллели и сортируем
-    classes_for_parallel = sorted(
-        set(class_name for class_name in class_column if class_name.startswith(selected_parallel)))
+    classes_for_parallel = sorted(set(class_name for class_name in class_column if class_name.startswith(selected_parallel)))
 
     # Создаем кнопки для выбора класса
     class_buttons = [
-        types.InlineKeyboardButton(class_name, callback_data=f"class_{user_id}_{class_name}") for class_name in
-        classes_for_parallel
+        types.InlineKeyboardButton(class_name, callback_data=f"class_{class_name}") for class_name in classes_for_parallel
     ]
 
     # Разбиваем кнопки по две в каждом столбце
@@ -145,25 +202,6 @@ def handle_parallel_selection(call):
 
     # Отправляем сообщение с Inline-клавиатурой для выбора класса
     bot.send_message(call.message.chat.id, "Выберите свой класс:", reply_markup=class_keyboard)
-
-
-# Декоратор для обработки Inline-кнопок
-@bot.callback_query_handler(func=lambda call: call.data.startswith('class_'))
-def handle_class_selection(call):
-    user_id, selected_class = call.data.split('_')[1:]
-
-    # Обрабатываем выбор класса
-    bot.send_message(call.message.chat.id, f"Вы выбрали класс: {selected_class}")
-
-    # Сохраняем состояние пользователя (выбранный класс)
-    save_user_state(user_id, selected_class)
-
-    # Получаем список учеников для выбранного класса
-    students_for_class = get_students_for_class(selected_class)
-
-    # Отправляем список учеников учителю
-    bot.send_message(call.message.chat.id,
-                     f"Список учеников в классе {selected_class}:\n" + "\n".join(students_for_class))
 
 
 # Декоратор для обработки текстовых сообщений
@@ -182,28 +220,8 @@ def handle_text(message):
         # Логика обработки других сообщений, если не выбран класс
         bot.send_message(chat_id, "Пожалуйста, выберите свой класс с помощью команды /choose_class")
 
-# Логика обработки выбора класса
-def handle_class_selection(chat_id, selected_class):
-    # Получаем список учеников для выбранного класса
-    students_for_class = get_students_for_class(selected_class)
 
-    # Отправляем список учеников учителю
-    bot.send_message(chat_id, f"Список учеников в классе {selected_class}:\n" + "\n".join(students_for_class))
-
-def get_students_for_class(selected_class):
-    class_column = worksheet.col_values(2)[1:]
-    name_column = worksheet.col_values(1)[1:]
-
-    # Получаем индексы учеников для выбранного класса
-    indices = [i for i, class_name in enumerate(class_column) if class_name == selected_class]
-
-    # Сортируем учеников по алфавиту на основе их индексов
-    sorted_students = sorted([(name_column[i], i) for i in indices])
-
-    # Возвращаем отсортированный список имен учеников
-    return [student[0] for student in sorted_students]
-
-
+# Запуск бота
 if __name__ == "__main__":
     # Создаем таблицы в базе данных, если их нет
     create_tables()
